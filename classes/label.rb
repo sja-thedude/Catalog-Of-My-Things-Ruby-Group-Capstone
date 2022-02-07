6ecab178e53b263c53ac873961baf88052d4d04f
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-02-07 16:03:25 +0300

    Added Tests for Book and Label

--- classes/label.rb
+++ classes/label.rb
@@ -1,5 +1,5 @@
 class Label
-  attr_accessor :title, :color
+  attr_accessor :id, :title, :color
   attr_reader :items
 
   def initialize(title, color)
@@ -10,7 +10,7 @@ class Label
   end
 
   def add_item(new_item)
-    @item << new_item
+    @items << new_item
     new_item.label = self
   end
 end
