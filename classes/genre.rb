3b8a1d0a7b27cdbbabf61ed16ec9fc0183306117
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-07 13:18:41 +0100

    Create music album class and define their properties

--- classes/genre.rb
+++ classes/genre.rb
@@ -2,7 +2,10 @@ class Genre
   attr_reader :items
   attr_accessor :name, :id
 
-  def initialize()
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
   
   end
 
--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,3 +1,8 @@
-class MusicAlbum
+require_relative './item'
 
+class MusicAlbum < Item
+  def initialize(on_spotify, publish_date = Time.now)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
 end
