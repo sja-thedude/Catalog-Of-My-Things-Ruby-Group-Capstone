50e40613f437848f96bf0a022ef52a220ac46694
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-07 14:56:16 +0100

    Adjust music album method

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,13 +1,15 @@
 require_relative './item'
-attr_accessor :on_spotify
+attr_accessor :publish_date, :on_spotify
 
 class MusicAlbum < Item
-  def initialize(on_spotify, publish_date = Time.now)
-    super(publish_date)
+  def initialize(published_date, on_spotify: false, archived: false)
+    super(published_date, archived: false)
     @on_spotify = on_spotify
+    @published_date = Date.parse(published_date)
+    @archived = archived
   end
 
   def can_be_archived?()
-    super() and @on_spotify
+    DateTime.now.year - @published_date.year > 10 && @on_spotify == true
   end
 end
