2a0eb3f5c88be221d72e2707264d0e0cd9060454
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-02-07 12:45:37 +0300

    Added book.rb , label.rb and preserver files

--- modules/preserver_module.rb
+++ modules/preserver_module.rb
@@ -1,1 +1,15 @@
-# Preserver module
+require 'json'
+
+module PreserverModule
+  def save_data_as_json(data, file_name)
+    File.write("./json/#{file_name}.json", JSON.generate(data)) if data.any?
+  end
+
+  def load_file(file_name)
+    if File.exist?("./json/#{file_name}.json")
+      JSON.parse(File.read("./json/#{file_name}.json"))
+    else
+      []
+    end
+  end
+end
