86f972adee2573b95990a0e56eceadc3e2582561
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-08 10:26:04 +0100

    Add a test for music album

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,1 +1,11 @@
-# tests
+require_relative '../classes/music_album'
+
+describe MusicAlbum do
+  before (:each) do
+    @musicalbum = MusicAlbum.new(false, 'artist', '03/04/1990')
+  end
+
+  it 'can has an instance' do
+    expect(@musicalbum).to be_an_instance_of MusicAlbum
+  end
+end
