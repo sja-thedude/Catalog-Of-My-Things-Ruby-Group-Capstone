c24ae44073a33cb19041269b47acb9c4970d483e
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-08 11:05:36 +0100

    Add a test for genre

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,1 +1,14 @@
-# tests
+require_relative '../classes/genre'
+require_relative '../classes/item'
+
+describe Genre do
+  before (:each) do
+    @genre = Genre.new('afrobeats')
+    @item = Item.new(@genre)
+  end
+
+  it 'has name to be afrobeats' do
+    expect(@genre.name).to eq 'afrobeats'
+  end
+
+end
